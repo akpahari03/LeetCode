6c3b6ac7de4d03249a0e60602160c21d4640bcfd
Ayush Kumar Pahari <103330068+akpahari03@users.noreply.github.com>
2024-08-21 17:02:25 +0530

    Time: 57 ms (26.24%), Space: 48.7 MB (72.78%) - LeetHub

--- 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
+++ 2813-to-be-or-not-to-be/2813-to-be-or-not-to-be.js
@@ -4,14 +4,13 @@
  */
 var expect = function(val) {
     return {
-        toBe:(val2)=>{
-            if(val!==val2)throw new Error("Not Equal");
-            else return true;
-        }
-        ,
-        notToBe:(val2)=>{
-            if(val===val2) throw new Error("Equal");
-            else return true;
+        toBe(val1) {
+            if(val===val1) return true;
+            else throw new Error("Not Equal")
+        },
+        notToBe(val1) {
+            if(val!==val1) return true;
+            else throw new Error("Equal")
         }
     }
 };
