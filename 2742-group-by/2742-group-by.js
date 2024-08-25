7dfa859dd56beb256d0968502f6bc34844add11a
Ayush Kumar Pahari <103330068+akpahari03@users.noreply.github.com>
2024-08-25 23:56:45 +0530

    Time: 119 ms (73.47%), Space: 70.5 MB (69.23%) - LeetHub

--- 2742-group-by/2742-group-by.js
+++ 2742-group-by/2742-group-by.js
@@ -3,14 +3,15 @@
  * @return {Object}
  */
 Array.prototype.groupBy = function(fn) {
-    return this.reduce((grouped,item)=> {
-        const key = fn(item)
-        if(!grouped[key]) {
-            grouped[key]=[]
-        }
-        grouped[key].push(item)
-        return grouped
-    },{})
+    // return this.reduce((grouped,item)=> {
+    //     const key = fn(item)
+    //     if(!grouped[key]) {
+    //         grouped[key]=[]
+    //     }
+    //     grouped[key].push(item)
+    //     return grouped
+    // },{})
+    return _.groupBy(this,fn)
 };
 
 /**
