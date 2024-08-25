7219aaee17e1d21e9543379b905fa1231b18d365
Ayush Kumar Pahari <103330068+akpahari03@users.noreply.github.com>
2024-08-25 23:33:34 +0530

    Time: 60 ms (33.72%), Space: 51.2 MB (77.7%) - LeetHub

---
+++ 2798-chunk-array/2798-chunk-array.js
@@ -0,0 +1,9 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function(arr, size) {
+    //using Lodash's _.chunk function
+    return _.chunk(arr,size)
+};
