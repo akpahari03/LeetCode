08f35227490708c795a5c4338d07e13c99f2e269
Ayush Kumar Pahari <103330068+akpahari03@users.noreply.github.com>
2024-08-28 03:00:50 +0530

    Time: 454 ms (16.33%), Space: 69.6 MB (5.51%) - LeetHub

--- 3069-change-data-type/3069-change-data-type.py
+++ 3069-change-data-type/3069-change-data-type.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
 def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
-    students['grade'] = students['grade'].astype(int)
+    #method 1
+    # students['grade'] = students['grade'].astype(int)
+    #method 2
+    students = students.astype({
+        'grade' : int
+    })
     return students
