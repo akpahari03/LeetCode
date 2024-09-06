634a6c2d401113387a349d130c45b59890e7873e
Ayush Kumar Pahari <103330068+akpahari03@users.noreply.github.com>
2024-09-07 01:24:14 +0530

    Time: 4 ms (47.24%), Space: 7.9 MB (82.59%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,15 +1,10 @@
+//USING STL
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int count=0;
-        int i = s.length()-1;
-        while(i>=0 && s[i] == ' ') {
-            i--;
-        }
-        while(i>=0 && s[i] != ' ') {
-            i--;
-            count++;
-        }
-        return count;
+        int idx = s.find_last_not_of(' ') + 1;
+        s.erase(idx);
+        int last_space_idx = s.find_last_of(' ');
+        return last_space_idx == string::npos ? s.length() : s.length() - last_space_idx - 1;
     }
 };
