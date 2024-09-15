cbe751ce07694fa88320892ec080244910b3a56f
Ayush Kumar Pahari <103330068+akpahari03@users.noreply.github.com>
2024-09-15 15:23:46 +0530

    Time: 78 ms (50.23%), Space: 18.9 MB (51.86%) - LeetHub

---
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findTheLongestSubstring(string s) {
+        unordered_map<int,int> mp;
+        int mask = 0;
+        mp[mask] = -1;
+        int ans = 0;
+        for(int i=0;i<s.length();i++) {
+            if(s[i]=='a') mask = (mask^(1<<0));
+            else if(s[i]=='e') mask = (mask^(1<<1));
+            else if(s[i]=='i') mask = (mask^(1<<2));
+            else if(s[i]=='o') mask = (mask^(1<<3));
+            else if(s[i]=='u') mask = (mask^(1<<4));
+
+            if(mp.find(mask) != mp.end()) {
+                ans = max(ans,i-mp[mask]);
+            } else mp[mask] = i;
+        }
+        return ans;
+    }
+};
