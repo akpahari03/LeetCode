17fdc3517c1e1849cebf4ebd3411ccd40f1c5aca
Ayush Kumar Pahari <103330068+akpahari03@users.noreply.github.com>
2024-09-21 02:00:35 +0530

    Time: 8 ms (43.2%), Space: 9.6 MB (93.07%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        string rev = s;
+        reverse(rev.begin(),rev.end());
+        for(int i=0;i<s.length();i++) {
+            if(!memcmp(s.c_str(), rev.c_str() + i, s.length()-i)) { //prefix in s == suffix in rev
+                return rev.substr(0, i) + s;
+            }
+        }
+        return rev + s;
+    }
+};
