ff6a8ddf260c4c614664a24157c9a39aadb6fd86
Ayush Kumar Pahari <103330068+akpahari03@users.noreply.github.com>
2024-10-11 09:51:28 +0530

    Time: 54 ms (44.04%), Space: 47.1 MB (25.09%) - LeetHub

--- 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
@@ -4,7 +4,6 @@ public:
         int n = nums.size();
         stack<int> s;
         
-        // Step 1: Build a decreasing stack of indices
         for (int i = 0; i < n; ++i) {
             if (s.empty() || nums[s.top()] > nums[i]) {
                 s.push(i);
@@ -12,8 +11,7 @@ public:
         }
         
         int maxWidth = 0;
-        
-        // Step 2: Traverse from the end and find maximum width ramp
+    
         for (int j = n - 1; j >= 0; --j) {
             while (!s.empty() && nums[s.top()] <= nums[j]) {
                 maxWidth = max(maxWidth, j - s.top());
