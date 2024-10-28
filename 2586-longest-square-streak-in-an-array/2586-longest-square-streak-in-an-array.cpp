4db12b0de0dd2aced7c017a666118661944ae2c1
Ayush Kumar Pahari <103330068+akpahari03@users.noreply.github.com>
2024-10-28 16:48:02 +0530

    Time: 115 ms (31.11%), Space: 108.9 MB (63.34%) - LeetHub

---
+++ 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int longestSquareStreak(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        sort(nums.begin(),nums.end());
+        int maxStreak = 0;
+        for(int& num : nums) {
+            int root = (int)sqrt(num);
+            if(root*root==num && mp.find(root) != mp.end()) {
+                mp[num] = mp[root]+1;
+            }
+            else  mp[num] = 1;
+            maxStreak = max(maxStreak,mp[num]);
+        }
+        return maxStreak < 2 ? -1 : maxStreak;
+    }
+};
