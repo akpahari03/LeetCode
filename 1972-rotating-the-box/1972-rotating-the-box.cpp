f778a65332d4eb9f6acd610e12cf2ada60a73fd7
Ayush Kumar Pahari <103330068+akpahari03@users.noreply.github.com>
2024-11-24 10:20:17 +0530

    Time: 235 ms (14.88%), Space: 56.4 MB (63.52%) - LeetHub

--- 1972-rotating-the-box/1972-rotating-the-box.cpp
+++ 1972-rotating-the-box/1972-rotating-the-box.cpp
@@ -1,45 +1,42 @@
 class Solution {
 public:
     vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
-         int m = box.size();
+        int m = box.size();
         int n = box[0].size();
 
-        vector<vector<char>> result(n, vector<char>(m));
+        vector<vector<char>> ans(n,vector<char>(m));
 
         //Transpose
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                result[i][j] = box[j][i];
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                ans[i][j] = box[j][i];
             }
         }
 
-        //90 degree k lie - Reverse kardo ahr row ko
-        for(vector<char>& row : result) {
-            reverse(begin(row), end(row));
+        //reverse every row
+        for(vector<char>& row : ans) {
+            reverse(row.begin(),row.end());
         }
 
-        //Apply the gravity
-        for(int j = 0; j < m; j++) {
-            for(int i = n-1; i >= 0; i--) {
-                if(result[i][j] == '.') { //if it is a space
+        for(int j=0;j<m;j++) {
+            for(int i=n-1;i>=0;i--) {
+                if(ans[i][j] == '.') {
                     int stoneRow = -1;
 
-                    for(int k = i-1; k >= 0; k--) {
-                        if(result[k][j] == '*') {
-                            break;
-                        } else if(result[k][j] == '#') {
+                    for(int k=i-1;k>=0;k--) {
+                        if(ans[k][j] == '*') break;
+                        else if(ans[k][j]=='#') {
                             stoneRow = k;
                             break;
                         }
                     }
-
                     if(stoneRow != -1) {
-                        result[i][j] = '#';
-                        result[stoneRow][j] = '.';
+                        ans[i][j]='#';
+                        ans[stoneRow][j]='.';
                     }
                 }
             }
         }
-        return result;
+        return ans;
     }
 };
