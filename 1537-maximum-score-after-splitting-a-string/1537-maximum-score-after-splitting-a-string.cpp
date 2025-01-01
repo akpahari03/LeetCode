5d2a512e7919f9cc16d99f2298078c012723538f
Ayush Kumar Pahari <103330068+akpahari03@users.noreply.github.com>
2025-01-01 22:59:22 +0530

    Time: 0 ms (100%), Space: 8.6 MB (52.6%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -7,15 +7,14 @@ public:
         for(int i=0;i<n;i++) {
             if(s[i]=='1') total_ones++;
         }
+        //int total_ones = count(begin(s), end(s), '1');  can be written in this form also
+        
+        int left_zeros = 0;
+        int left_ones = 0;
         for(int i=0;i<n-1;i++) {
-            int left_zeros = 0;
-            int left_ones = 0;
-            int right_ones = 0;
-            for(int j=0;j<=i;j++) {
-                if(s[j]=='0') left_zeros++;
-                else  left_ones++;
-            }
-            right_ones = total_ones-left_ones;
+            if(s[i]=='0') left_zeros++;
+            else left_ones++;
+            int right_ones = total_ones-left_ones;
             maxi = max(maxi,left_zeros+right_ones);
         }
         return maxi;
