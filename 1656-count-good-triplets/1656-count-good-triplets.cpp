dc607e089c999c402511bc72dad22d7af6065902
Ayush Kumar Pahari <103330068+akpahari03@users.noreply.github.com>
2025-01-04 08:21:06 +0530

    Time: 9 ms (41.08%), Space: 11.1 MB (25.54%) - LeetHub

--- 1656-count-good-triplets/1656-count-good-triplets.cpp
+++ 1656-count-good-triplets/1656-count-good-triplets.cpp
@@ -5,8 +5,9 @@ public:
         int count = 0;
         for(int i=0;i<n-2;i++) {
             for(int j=i+1;j<n-1;j++) {
+                if(abs(arr[i] - arr[j]) > a) continue;
                 for(int k=j+1;k<n;k++) {
-                    if(abs(arr[i]-arr[j])<=a && abs(arr[j]-arr[k])<=b && abs(arr[i]-arr[k])<=c) count++;
+                    if(abs(arr[j]-arr[k])<=b && abs(arr[i]-arr[k])<=c) count++;
                 }
             }
         }
