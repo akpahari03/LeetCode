3b0d189ba45f5988651e3ad3e82b8624a7ef7221
Ayush Kumar Pahari <103330068+akpahari03@users.noreply.github.com>
2025-01-09 18:59:07 +0530

    Time: 0 ms (100%), Space: 13.3 MB (60.46%) - LeetHub

--- 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.cpp
@@ -5,6 +5,7 @@ public:
         int m = pref.length();
         int count = 0;
         for(int i=0;i<n;i++) {
+            if(m>words[i].length()) continue;
             if(words[i].substr(0,m)==pref) count++;
         }
         return count;
