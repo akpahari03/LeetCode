868691bf4c2c6681f601ab2520ac7229075d61ed
Ayush Kumar Pahari <103330068+akpahari03@users.noreply.github.com>
2025-02-11 21:27:51 +0530

    Time: 14 ms (5.85%), Space: 25.4 MB (5.32%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,10 +1,35 @@
 class Solution {
 public:
-    string removeOccurrences(string s, string part) {
-        while(s.length()!=0 && s.find(part)<s.length())
-        {
-            s.erase(s.find(part),part.length());
+    bool check(stack<char>& st,string& part ,int m) {
+        stack<char> tempSt = st;
+        for(int idx=m-1 ;idx>=0 ;idx--) {
+            if(tempSt.top() != part[idx]) {
+                return false;
+            }
+            tempSt.pop();
         }
-        return s;
+        return true;
+    }
+    string removeOccurrences(string s, string part) {
+        stack<char> st;
+        int n = s.length();
+        int m = part.length();
+
+        for(int i=0;i<n;i++) {
+            st.push(s[i]);
+
+            if(st.size() >= m && check(st,part,m)==true) {
+                for(int j=0;j<m;j++) {
+                    st.pop();
+                }
+            }
+        }
+        string ans = "";
+        while(!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
