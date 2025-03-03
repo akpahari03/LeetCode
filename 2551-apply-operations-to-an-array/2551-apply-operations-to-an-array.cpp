74a3b04dd38349b50d5298cea45b25ba2e2cd897
Ayush Kumar Pahari <103330068+akpahari03@users.noreply.github.com>
2025-03-04 04:12:40 +0530

    Time: 0 ms (100%), Space: 13.2 MB (61.09%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -2,28 +2,22 @@ class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
         int n = nums.size();
-        vector<int> modifiedNums;
 
-        // Step 1: Apply operations on the array
-        for (int index = 0; index < n - 1; index++) {
-            if (nums[index] == nums[index + 1] && nums[index] != 0) {
-                nums[index] *= 2;
-                nums[index + 1] = 0;
+        int j = 0;
+        for(int i = 0; i < n; i++) {
+            if(i+1 < n && nums[i] == nums[i+1] && nums[i] != 0) {
+                nums[i] *= 2;
+                nums[i+1] = 0;
+            }
+
+            if(nums[i] != 0) {
+                if(i != j) {
+                    swap(nums[i], nums[j]);
+                }
+                j++;
             }
         }
 
-        // Step 2: Move non-zero elements to the front
-        for (int num : nums) {
-            if (num != 0) {
-                modifiedNums.push_back(num);
-            }
-        }
-
-        // Step 3: Append zeros to maintain the original size
-        while (modifiedNums.size() < n) {
-            modifiedNums.push_back(0);
-        }
-
-        return modifiedNums;
+        return nums;
     }
 };
