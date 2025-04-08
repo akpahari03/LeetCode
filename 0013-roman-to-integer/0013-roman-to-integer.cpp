742da58a50c988cfd9b5ddae271dfdeb44a8732c
Ayush Kumar Pahari <103330068+akpahari03@users.noreply.github.com>
2025-04-08 16:14:58 +0530

    Time: 7 ms (49.52%), Space: 12.3 MB (59.99%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -10,9 +10,11 @@ public:
         mp['D'] = 500;
         mp['M'] = 1000;
 
-        int ans = mp[s[s.length()-1]];
-        for(int i = s.length()-2 ; i>=0 ; i--) {
-            if(mp[s[i]] < mp[s[i+1]]) ans -= mp[s[i]];
+        int ans = 0;
+        for(int i=0;i<s.length();i++) {
+            if(mp[s[i]]<mp[s[i+1]]) {
+                ans -= mp[s[i]];
+            }
             else ans += mp[s[i]];
         }
         return ans;
