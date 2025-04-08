8b1932b76477384df8646d58479dca1eab1d017e
Ayush Kumar Pahari <103330068+akpahari03@users.noreply.github.com>
2025-04-09 00:11:50 +0530

    Time: 6 ms (8.57%), Space: 9.4 MB (100%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,10 +1,50 @@
 class Solution {
 public:
-    bool isValid()
-    bool isPalindrome(string s) {
-        string temp = "";
-        for(char& ch : s) {
-            if(valid(ch)) temp.push_back(ch);
+    bool valid(char ch)
+    {
+        if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z') || (ch>='0' && ch<='9'))
+        {
+            return 1;
         }
+        return 0;
+    }
+    char lowerCase(char ch)
+    {
+        if((ch>='a' && ch<='z')) return ch;
+        else
+        {
+            char temp=ch-'A'+'a';
+            return temp;
+        }
+    }
+    bool checkPalindrom(string s)
+    {
+        int i=0;
+        int j=s.length()-1;
+        while(i<=j)
+        {
+            if(s[i]!=s[j]) return false;
+            else
+            {
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+    bool isPalindrome(string s) {
+        string temp="";
+        for(int i=0;i<s.length();i++)
+        {
+            if(valid(s[i]))
+            {
+                temp.push_back(s[i]);
+            }
+        }
+        for(int i=0;i<temp.length();i++)
+        {
+            temp[i]=lowerCase(temp[i]);
+        }
+        return checkPalindrom(temp);
     }
 };
