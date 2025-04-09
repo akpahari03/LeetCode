6c8180e99e798b5b1d7dbf14397e892abd1625f6
Ayush Kumar Pahari <103330068+akpahari03@users.noreply.github.com>
2025-04-09 16:17:47 +0530

    Time: 0 ms (100%), Space: 10.6 MB (9.61%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,50 +1,38 @@
 class Solution {
 public:
-    bool valid(char ch)
-    {
-        if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z') || (ch>='0' && ch<='9'))
-        {
-            return 1;
+    bool valid(char ch) {
+        if((ch>='A' && ch<='Z') || (ch>='a' && ch<='z')|| (ch>='0' && ch<='9')) {
+            return true;
         }
-        return 0;
+        return false;
     }
-    char lowerCase(char ch)
-    {
-        if((ch>='a' && ch<='z')) return ch;
-        else
-        {
-            char temp=ch-'A'+'a';
+    char lowercase(char ch) {
+        if(ch>='a' && ch<='z') return ch;
+        else {
+            char temp = ch-'A'+'a';
             return temp;
         }
     }
-    bool checkPalindrom(string s)
-    {
+    bool checkPalindrome(string s) {
         int i=0;
-        int j=s.length()-1;
-        while(i<=j)
-        {
+        int j = s.length()-1;
+        while(i<=j) {
             if(s[i]!=s[j]) return false;
-            else
-            {
-                i++;
-                j--;
-            }
+            i++;
+            j--;
         }
         return true;
     }
     bool isPalindrome(string s) {
-        string temp="";
-        for(int i=0;i<s.length();i++)
-        {
-            if(valid(s[i]))
-            {
+        string temp = "";
+        for(int i=0;i<s.length();i++) {
+            if(valid(s[i])) {
                 temp.push_back(s[i]);
             }
         }
-        for(int i=0;i<temp.length();i++)
-        {
-            temp[i]=lowerCase(temp[i]);
+        for(int i=0;i<temp.length();i++) {
+            temp[i] = lowercase(temp[i]);
         }
-        return checkPalindrom(temp);
+        return checkPalindrome(temp);
     }
 };
