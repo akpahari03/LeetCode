d824bf774d88abb77c7cac19401e51a4411b3a6f
Ayush Kumar Pahari <103330068+akpahari03@users.noreply.github.com>
2025-04-12 16:00:10 +0530

    Time: 2 ms (35.77%), Space: 17 MB (42.69%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,44 +1,47 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        int vis[n][m];
-        int cntFresh = 0;
-        queue<pair<pair<int,int> , int>> q;
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<m;j++) {
-                if(grid[i][j] == 2) {
-                    q.push({{i,j},0});
-                    vis[i][j]=2;
+        int m = grid.size();
+        int n = grid[0].size();
+
+        queue<pair<int,int>> q;
+        int freshCount = 0;
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if(grid[i][j]==2) {
+                    q.push({i,j});
                 }
-                else {
-                    vis[i][j] = 0;
+                else if(grid[i][j]==1) {
+                    freshCount++;
                 }
-                if(grid[i][j] == 1) cntFresh++;
             }
         }
-        int tm = 0;
-        int drow[] = {-1,0,1,0};
-        int dcol[] = {0,1,0,-1};
-        int cnt = 0;
+        if(freshCount==0) return 0;
+        vector<vector<int>> directions{{-1,0},{1,0},{0,-1},{0,1}};
+        int time = 0;
         while(!q.empty()) {
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int t = q.front().second;
-            tm = max(t,tm);
-            q.pop();
-            for(int i=0;i<4;i++) {
-                int nrow = r + drow[i];
-                int ncol = c + dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol] == 1 && vis[nrow][ncol] == 0) {
-                    q.push({{nrow,ncol},t+1});
-                    vis[nrow][ncol] = 2;
-                    cnt++;
-                }
+            int N = q.size();
+            while(N--) {
+                pair<int,int> curr = q.front();
+                q.pop();
+                int i = curr.first;
+                int j = curr.second;
+
+                for(auto &dir : directions) {
+                    int new_i = i+dir[0];
+                    int new_j = j+dir[1];
+
+                    if(new_i>=0 && new_i<m && new_j>=0 && new_j<n && grid[new_i][new_j]==1) {
+                        grid[new_i][new_j]=2;
+                        q.push({new_i,new_j});
+                        freshCount--;
+                    }
+                } 
             }
+            time++;
         }
-        if(cnt != cntFresh) return -1;
-        return tm;
+        if(freshCount==0) return (time-1);
+        return -1;
     }
+    
 };
